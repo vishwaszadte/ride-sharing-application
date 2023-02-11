32bcc08a8d6667b4a8f84fcf1d5900c92548c6fc
Vishwas <vishwaszadte@gmail.com>
2023-02-11 11:19:13 +0530

    Add database connection initialization

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const mongoURL = dotenv.parsed.MONGO_URL;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("db is live");
+  })
+  .catch((e) => {
+    console.log(e);
+    console.log("error connecting to the db");
+  });

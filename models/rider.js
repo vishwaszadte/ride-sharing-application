d080e197a3eb8f1f54caf4cb9f40048c3f6afc94
Vishwas <vishwaszadte@gmail.com>
2023-02-11 11:19:42 +0530

    Add rider schama

---
+++ models/rider.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const riderSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email address: " + value);
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+    validate(value) {
+      if (value.toLowerCase().includes("password") || value.includes("1234")) {
+        throw new Error("Weak password");
+      }
+    },
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+    minlength: 9,
+  },
+});
+
+module.exports = Rider = mongoose.model("Rider", riderSchema);

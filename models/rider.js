5b63012ecd69f79c715c3d5f84fbc58b74c68de3
Vishwas <vishwaszadte@gmail.com>
2023-03-08 20:30:54 +0530

    Update Rider model to include default values for location

--- models/rider.js
+++ models/rider.js
@@ -37,6 +37,14 @@ const riderSchema = mongoose.Schema({
   },
   location: {
     type: locationSchema,
+    default: {
+      formattedAddress: "RSCOE, Tathawade",
+      latitude: 18.632096514025942,
+      longitude: 73.84571163425848,
+      city: "Pune",
+      country: "India",
+      pincode: "411033",
+    },
     required: true,
   },
 });

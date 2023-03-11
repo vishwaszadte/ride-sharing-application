e868e93442e5e4cc6ec2c63196ea448587a3fc3c
Vishwas <vishwaszadte@gmail.com>
2023-03-11 13:58:31 +0530

    Update /driver/signup route to upload user input photo to s3

--- routes/driver.js
+++ routes/driver.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Driver = require("../models/driver");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const fileUpload = require("express-fileupload");
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
+});
+
+// defining the upload variable for the configuration of photo being uploaded
+const upload = multer({ storage: storage });
 
 const router = new express.Router();
 router.use(express.json());
@@ -42,18 +54,56 @@ router
   .get((req, res) => {
     res.render("driver/signup", { error: "" });
   })
-  .post(async (req, res) => {
-    const driver = new Driver(req.body);
+  .post(upload.single("photo"), (req, res) => {
+    // S3 instance to upload photo to bucket
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID, // accessKeyId that is stored in .env file
+      secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET, // secretAccessKey is also store in .env file
+    });
 
-    try {
-      await driver.save();
-      res.status(201).render("driver/login", {
-        error: "User created successfully. Please log in.",
+    // Definning the params variable to uplaod the photo
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME, // bucket that we made earlier
+      Key: req.file.originalname, // Name of the image
+      Body: req.file.buffer, // Body which will contain the image in buffer format
+      ACL: "public-read-write", // defining the permissions to get the public link
+      ContentType: "image/jpeg", // Necessary to define the image content-type to view the photo in the browser with the link
+    };
+
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ error: err }); // if we get any error while uploading error message will be returned.
+        return;
+      }
+      // If not then below code will be executed
+
+      console.log(data);
+
+      const driver = new Driver({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        phoneNumber: req.body.phoneNumber,
+        vehicleName: req.body.vehicleName,
+        vehicleNumber: req.body.vehicleNumber,
+        vehicleType: req.body.vehicleType,
+        photo: data.Location,
       });
-    } catch (err) {
-      console.log(err);
-      res.render("driver/signup", { error: err });
-    }
+
+      driver
+        .save()
+        .then((result) => {
+          res.status(201).render("driver/login", {
+            error: "User created successfully. Please log in.",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("driver/signup", { error: err });
+        });
+    });
   });
 
 module.exports = router;
